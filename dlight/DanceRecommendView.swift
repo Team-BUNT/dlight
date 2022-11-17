3cf338d13e3bb6d8f1437263201c3604f1490f52
Apple-Kong <kongthn@gmail.com>
2022-11-17 19:17:56 +0900

    [FEAT] player scroll enable

--- dlight/DanceRecommendView.swift
+++ dlight/DanceRecommendView.swift
@@ -14,21 +14,26 @@ struct DanceRecommendView: View {
     @State var top = 0
     @State var data: [Video] = []
 
-    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            PlayerScrollView(data: self.$data)
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Capsule()
+                }
+            }
+        }
     }
 }
 
 struct DanceRecommendView_Previews: PreviewProvider {
     static var previews: some View {
-        DanceRecommendView(data: [
-            Video(id: 0, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video4", ofType: "mp4")!)), replay: false),
-            Video(id: 1, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video5", ofType: "mp4")!)), replay: false),
-            Video(id: 2, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video2", ofType: "mp4")!)), replay: false),
-            Video(id: 3, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video3", ofType: "mp4")!)), replay: false),
-            Video(id: 4, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video1", ofType: "mp4")!)), replay: false),
-            Video(id: 5, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video6", ofType: "mp4")!)), replay: false),
-        ])
+        DanceRecommendView()
     }
 }
