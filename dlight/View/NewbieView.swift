60a10bea7052d6fe4c1e5fb0a54f6ff966e2bcb8
J2in <jiin62622@gmail.com>
2022-11-17 23:38:09 +0900

    [add] make NewbieView
    add NewbieView.swift file

---
+++ dlight/View/NewbieView.swift
@@ -0,0 +1,79 @@
+//
+//  NewbieView.swift
+//  dlight
+//
+//  Created by jiin on 2022/11/17.
+//
+
+import SwiftUI
+
+struct NewbieView: View {
+    
+    let choice = ["음악을 듣고 가볍게 리듬을 타고 싶어서", "친구들과 영상을 찍기위해", "멋진 무대에서 공연해보고 싶어서"]
+    
+    @State var selectedItems: [String] = []
+    // 선택한 선택지를 담는 어레이 구현 필요 [ ]
+    var body: some View {
+        NavigationView {
+            VStack {
+                VStack {
+                    Text("Q.")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    Text("왜 춤을 배워보고 싶으신가요?")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                .padding(.vertical, 80)
+            
+                VStack {
+                    ForEach(0..<3) { index in
+                        PreferenceButtonView(text: choice[index], items: $selectedItems)
+                    }
+                }
+                
+                Spacer()
+                
+                NavigationLink {
+                    MusicPreferenceView()
+                } label: {
+                    Text("Next")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                .buttonStyle(PreferenceButtonStyle())
+                
+                
+            }
+            .padding(.horizontal, 20)
+            .navigationBarHidden(true)
+            
+        }
+        .background(Color("background"))
+        
+    }
+}
+
+
+//버튼 애니메이션
+struct PreferenceButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .frame(height: 60)
+            .frame(maxWidth: .infinity)
+            .background(configuration.isPressed ? Color("Primary") : Color(Color.white))
+            .cornerRadius(10)
+//            .scaleEffect(configuration.isPressed ? 1.3 : 1.0)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
