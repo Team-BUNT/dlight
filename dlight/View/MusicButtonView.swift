0c9500d7cefd358337d0fc9423327520961c9436
J2in <jiin62622@gmail.com>
2022-11-18 02:15:48 +0900

    add Luke's NC1
    add Luke's NC1

---
+++ dlight/View/MusicButtonView.swift
@@ -0,0 +1,76 @@
+//
+//  MusicButtonView.swift
+//  dlight
+//
+//  Created by jiin on 2022/11/18.
+//
+
+
+import SwiftUI
+
+struct MusicButtonView: View {
+    
+    let item: MusicGenre
+    
+    @Binding var items: [MusicGenre]
+    
+    
+    var body: some View {
+        
+        Button {
+            
+            //ViewModel 로 이사가야할 로직 🚧
+            if items.contains(item) {
+                
+                items.removeAll { $0 == item }
+            } else {
+                if items.count < 3 {
+                    items.append(item)
+                }
+            }
+            print(items)
+        } label: {
+            ZStack {
+                Image(item.rawValue)
+                    .resizable()
+                    .clipShape(
+                        DiagonalFrame()
+                    )
+                
+                VStack {
+                    HStack {
+                        Text(item.koreanText)
+                            .font(.body)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .padding(20)
+    
+                        Spacer()
+                    }
+                    Spacer()
+                }
+            }
+        }
+        .aspectRatio(16 / 14, contentMode: .fit)
+        .background(Color("Container"))
+        .cornerRadius(10)
+//        .if(items.contains(item)) { view in
+//            view.overlay(
+//                RoundedRectangle(cornerRadius: 10)
+//                    .stroke(lineWidth: 1.5)
+//                    .fill(Color("AccentColor"))
+//            )
+//        }
+    }
+}
+
+
+struct MusicButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        MusicButtonView(item: .Boombap, items: .constant([]))
+            .frame(width: 180, height: 160)
+            .previewLayout(.sizeThatFits)
+            
+    }
+}
+
---
+++ dlight/View/MusicPreferenceView.swift
@@ -0,0 +1,79 @@
+//
+//  MusicPreferenceView.swift
+//  dlight
+//
+//  Created by jiin on 2022/11/18.
+//
+
+
+import SwiftUI
+
+struct MusicPreferenceView: View {
+    //간격 한번에 설정하기 위한 변수
+    var totalSpacing: CGFloat = 20
+    
+    @State var selectedItems: [MusicGenre] = []
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                ScrollView {
+                    LazyVGrid(columns: [
+                        GridItem(.flexible(), spacing: totalSpacing),
+                        GridItem(.flexible())
+                    ], spacing: totalSpacing
+                    ) {
+                        ForEach(MusicGenre.allCases, id: \.self) { item in
+                            MusicButtonView(item: item, items: $selectedItems)
+                        }
+                    }
+                    .padding(.horizontal, totalSpacing)
+                    .padding(.top, 20)
+                }
+                .background(Color("Background"))
+                
+                
+                VStack {
+                    NavigationLink {
+                      //  VideoView()
+                    } label: {
+                        Text("Next")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                    }
+                    .buttonStyle(PreferenceButtonStyle())
+                    .padding(.horizontal, 20)
+                
+                }
+                .frame(height: 80)
+            
+            }
+            
+            
+//            VStack {
+//                Spacer()
+//
+//
+//            }
+        }
+        .navigationBarBackButtonHidden(true)
+        .background(Color("Background"))
+        .navigationTitle(
+            Text("음악 취향")
+        )
+    }
+}
+
+
+
+
+
+struct MusicPreferenceView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            MusicPreferenceView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
