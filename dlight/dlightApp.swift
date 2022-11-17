bb02c82b28c745244b02c18ce06295f535980bab
Apple-Kong <kongthn@gmail.com>
2022-11-17 16:51:36 +0900

    Initial Commit

---
+++ dlight/dlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  dlightApp.swift
+//  dlight
+//
+//  Created by GOngTAE on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct dlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
