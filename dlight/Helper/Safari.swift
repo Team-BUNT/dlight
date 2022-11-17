44fb977221e1f49be311c20c55a6c44e7fc0e0fc
Bethev <jmail88@naver.com>
2022-11-18 00:55:47 +0900

    [Feat] 온보딩뷰

--- dlight/Helper/Safari.swift
+++ dlight/Helper/Safari.swift
@@ -8,21 +8,11 @@
 import SwiftUI
 import WebKit
 
-struct WebView: UIViewRepresentable {
-    var urlToLoad: String
-    
-    func makeUIView(context: Context) -> WKWebView {
-        guard let url = URL(string: self.urlToLoad) else { return WKWebView() }
-        
-        let webView = WKWebView()
-        webView.backgroundColor = .black
-        
-        webView.load(URLRequest(url: url))
-        return webView
-    }
-    
-    func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<WebView>) {
-        
+struct Safari {
+    static func open(target: StudioName) {
+        if let url = URL(string: target.url) {
+            UIApplication.shared.open(url)
+        }
     }
 }
 
